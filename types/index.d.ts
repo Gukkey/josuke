2e43d72bf6c63ea6c63cc8ebbef2fa94865ecbb1
Guhan <74915094+Gukkey@users.noreply.github.com>
2025-09-14 23:38:14 +0530

    refactor: update ts type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,6 @@
-export function validate(emailAddress: string): boolean;
+/**
+ *
+ * @param {string|string[]} emailAddress - String or Array of email addresses
+ * @returns {boolean} true if email or all emails in array are valid else false
+ */
+export function validate(emailAddress: string | string[]): boolean;
